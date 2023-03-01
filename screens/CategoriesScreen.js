99485f839f1e4ce15656ec0474307880f2689e33
user name <user email>
2023-03-01 12:10:07 +0300

    implementing navigation

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,12 +3,19 @@ import CategoryGridTile from "../components/CategoryGridTile";
 
 import { CATEGORIES } from "../data/dummy-data";
 
-function  renderCategoryItem(item) {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} numColumns={2}/>;
+function CategoriesScreen({ navigation }) {
+    function  renderCategoryItem(itemData) {
+        function pressHandler() {
+            navigation.navigate('MealsOverview', {
+                categoryId: itemData.item.id,
+            });
+        }
+
+    return (<CategoryGridTile title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} navigation={navigation}/>);
 }
 
-function CategoriesScreen() {
-    return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} />
+
+    return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} numColumns={2}/>
 }
 
 export default CategoriesScreen;
